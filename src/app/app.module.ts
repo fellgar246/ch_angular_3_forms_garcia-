fd014e8438ca2f798e36260085ab2ab67898cd96
fellgar246 <fellgar246@gmail.com>
2023-07-05 20:35:59 -0600

    register config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { ContentComponent } from './content/content.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
 
 @NgModule({
@@ -13,11 +13,11 @@ import { RegisterFormComponent } from './register-form/register-form.component';
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-    ContentComponent,
     RegisterFormComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+interface RegisterModel {
+  name: FormControl<string | null>;
+  lastName: FormControl<string | null>;
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
 
 @Component({
   selector: 'app-register-form',
@@ -6,5 +14,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent {
+
+  mostrarAlert: boolean = false;
+
+  nameControl = new FormControl('');
+  lastNameControl = new FormControl('');
+  emailControl = new FormControl('', [Validators.required]);
+  passwordControl = new FormControl('');
+
+  registerModel: FormGroup<RegisterModel> = new FormGroup({
+    name: this.nameControl,
+    lastName: this.lastNameControl,
+    email: this.emailControl,
+    password: this.passwordControl
+  });
+
+
+
+  submitRegistro() {
+    if (this.registerModel.valid) {
+      // Realizar acciones de registro aquí
+      this.mostrarAlert = true;
+
+      setTimeout(() => {
+        this.mostrarAlert = false;
+      }, 2000);
+
+      console.log('Registro exitoso');
+      console.log(this.registerModel.value);
+
+
+    }
+  }
 
 }
